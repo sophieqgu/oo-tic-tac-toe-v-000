d158a724716d726368ae6cdaecb4ea8e40795cb3
Sophie Gu <sophieqgu@gmail.com>
2018-08-28 04:24:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,8 +3,7 @@ class TicTacToe
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]  
   end 
   
-  def WIN_COMBINATIONS
-    WIN_COMBINATIONS = [
+  def WIN_COMBINATIONS = [
       [0,1,2],
       [3,4,5],
       [6,7,8],
