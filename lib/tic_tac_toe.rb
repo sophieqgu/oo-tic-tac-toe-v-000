43335cb6535419b6566363954210900a83ba30bb
Sophie Gu <sophieqgu@gmail.com>
2018-08-28 04:34:46 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,7 +52,7 @@ class TicTacToe
     index = input_to_index(input)
     if valid_move?(index)
       move(index, token = current_player)
-      display_board(@board)
+      display_board
     else 
       puts "The position is already taken."
       input = gets.strip
